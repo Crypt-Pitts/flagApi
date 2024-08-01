d4fd885cc4a03839910300f5af1cd0f780350ca1
Crypt-Pitts <95093836+Crypt-Pitts@users.noreply.github.com>
2024-08-01 09:50:13 -0400

    flag cards connected to data

--- index.js
+++ index.js
@@ -10,46 +10,34 @@ function darkmodeHandler() {
 
 
 
-//Example fetch using pokemonapi.co
-// document.querySelector('button').addEventListener('click', getFetch)
+const postContainer = document.querySelector('.card-container');
 
-// document.querySelector('h2').innerText = localStorage.getItem('books')
 
-// const input = document.querySelector('input')
 
-// input.addEventListener('keydown', (event) => {
-//   if (event.key === 'Enter') {
-//     event.preventDefault(); // prevent default behavior
-//     getFetch(); // submit the form
-    
-//   }
-// });
-
-const card = document.getElementById('card')
-let div = document.createElement('div')
 async function getFetch(){
     try{
         const url = `./data.json`
         const res = await fetch(url);
         const data = await res.json()
         console.log(data)
-        data.forEach(country => {
-            
-            div.append(card.insertAdjacentHTML("beforebegin",`<img src="${country.flag}" alt="">`))
-            div.append(card.insertAdjacentHTML("beforebegin",`<h2>${country.name}</h2>`) )
-            div.append(card.insertAdjacentHTML("beforebegin",`<p>Population: ${country.population}</p>`))
-            div.append(card.insertAdjacentHTML("beforebegin",`<p>Region: ${country.region}</p>`))
-            div.append(card.insertAdjacentHTML("beforebegin",`<p>Capitol: ${country.capital}</p>`)     )
+        const postMethods = () => {
+            data.map((e)=>{
+                const postEl = document.createElement('div');
+                postEl.classList.add('card');
+                postEl.innerHTML = `
+                      <img src=${e.flag} alt="" class="card-flag">
+                      <h2 class="card-name">${e.name}</h2>
+                      <p class="card-pop">Population: ${e.population}</p>
+                      <p class="card-region">Region: ${e.region}</p>
+                      <p class="card-cap">Capital: ${e.capital}</p>
+                `
+                postContainer.appendChild(postEl)
+            })
+        }
+        postMethods();
 
-            // card.insertAdjacentHTML("beforeend",`<img src="${country.flag}" alt="">`)
-            // card.insertAdjacentHTML("beforeend",`<h2>${country.name}</h2>`) 
-            // card.insertAdjacentHTML("beforeend",`<p>Population: ${country.population}</p>`)
-            // card.insertAdjacentHTML("beforeend",`<p>Region: ${country.region}</p>`)
-            // card.insertAdjacentHTML("beforeend",`<p>Capitol: ${country.capital}</p>`)
-            // card.append()
-    })
-        // data.forEach(pic=> listEl.insertAdjacentHTML('beforeend',`<img src="${pic.flag}" alt="">`))
 
+    
 }   catch(err){
           console.log(`error ${err}`)
       } ;
